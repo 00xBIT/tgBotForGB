737026b445ebf15fdda9bf9fcd4446030951e6aa
00xBIT <4498580+00xBIT@users.noreply.github.com>
2018-10-11 02:25:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,9 +10,25 @@ bot = telebot.TeleBot(TOKEN)
 
 bot.send_message(MY_ID, 'START!')
 
+def buildDebugMessage(message):
+    now = datetime.datetime.now()
+    now = now.strftime("%d-%m-%Y %H:%M:%S")
+    contentType = str(message.content_type)
+    string = "%s %s %s (@%s id: %i)\nContentType: %s\n" % (now, message.chat.first_name, message.chat.last_name, message.chat.username, message.chat.id, contentType)
+    if contentType == 'text':
+        string += 'Text: %s' % (message.text)
+
+    return string
+
 @bot.message_handler(func=lambda message: True)
 def incoming_message(message):
     print(message.text)
+    
+    #Forward all messages to MY_ID
+    if message.chat.id != MY_ID:
+        string = buildDebugMessage(message)
+        bot.send_message(MY_ID, string)
+
     if "подтверди" in message.text:
         bot.reply_to(message, "подтверждаю")
     else:
