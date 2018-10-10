bb034e13800093ec3e8c1fd2f6005e8f3a96ed06
00xBIT <4498580+00xBIT@users.noreply.github.com>
2018-10-11 03:11:27 +0700

    Update bot.py
    Понимает стикеры и отвечает на них тем же стикером

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ TOKEN = ''
 #paste your telegram ID - for receive debug message
 MY_ID = 0000000
 
+STICKER_PATH = 'stickers'
+
 bot = telebot.TeleBot(TOKEN)
 
 bot.send_message(MY_ID, 'START!')
@@ -20,11 +22,41 @@ def buildDebugMessage(message):
 
     return string
 
+@bot.message_handler(content_types=['sticker'])
+def handle_sticker(message):
+    bot.reply_to(message, "это стикер")
+    
+    # Check folder for download
+    try:
+        if not os.path.exists(STICKER_PATH):
+            os.makedirs(STICKER_PATH)
+    except Exception as e:
+        print(e)
+
+    #Save stiker to localdisk
+    try:
+        file_info = bot.get_file(message.sticker.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+		# src='/mnt/files/tmp/'+file_info.file_path;
+        src = file_info.file_path;
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+            
+        print("stiker downloaded")
+
+
+        #Send file
+        bot.send_document(message.chat.id, open(src, 'rb'))
+        print("stiker sent")
+    except Exception as e:
+        print(e)
+
 @bot.message_handler(func=lambda message: True)
 def incoming_message(message):
     print(message.text)
-    
-    #Forward all messages to MY_ID
+
+    # Пересылать сообщения от всех пользователей -> админу в чат
     if message.chat.id != MY_ID:
         string = buildDebugMessage(message)
         bot.send_message(MY_ID, string)
